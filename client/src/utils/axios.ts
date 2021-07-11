7fe11a63d93aa02662f64da22421c83d1edf3650
Kedar Karki <kkboss12@outlook.com>
2021-07-11 21:52:28 +0545

    l

--- client/src/utils/axios.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-export const baseURL = 'http://3.232.224.65:8080';
-export const clientUrl = 'http://3.232.224.65:8080';
-const instance = axios.create({
-baseURL,
-});
-
-export default instance;
